c90d2a8448cc380d8095483ab883ca2b341b6820
Alan Chen <github@zenan.ch>
2024-10-21 17:54:10 -0700

    animation from https://github.com/samuelkraft/samuelkraft-next/pull/167

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { ReactNode } from "react";
 import cn from "clsx";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 type NavLinkProps = {
   href: string;
@@ -16,12 +17,19 @@ export default function NavLink({ href, children }: NavLinkProps) {
   return (
     <Link
       className={cn(
-        "px-3 py-2 rounded-full text-sm hover:text-primary transition-colors",
-        active ? "bg-secondaryA text-primary" : "text-secondary"
+        "relative px-4 py-2 rounded-full text-sm hover:text-primary transition-colors",
+        active ? "text-primary" : "text-secondary"
       )}
       href={href}
     >
       {children}
+      {active && (
+        <motion.div
+          layoutId="underline"
+          className="absolute left-0 top-0 bottom-0 right-0 -z-20 bg-secondaryA rounded-full"
+          transition={{ duration: 0.35 }}
+        />
+      )}
     </Link>
   );
 }
