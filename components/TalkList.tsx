1bd2f01c9f7a84fb3e9bbe74f767753a0d59c5f2
kunli <kunli@microsoft.com>
2025-08-05 19:32:55 +0800

    update

--- components/TalkList.tsx
+++ components/TalkList.tsx
@@ -3,8 +3,8 @@ import Link from "./Link";
 import { formatDate } from "../lib/formatdate";
 
 export interface Talk {
-  title: string;
-  conference: string;
+  title?: string;
+  conference?: string;
   date: string;
   location: string;
   link?: string;
--- data/talks.ts
+++ data/talks.ts
@@ -1,29 +1,103 @@
 export const talks = [
-  {
-    conference: "Workshop on Information Technologies and Systems (WITS)",
+  { 
     title: "Towards E-Level Scalable Scientific Intelligence Systems",
     location: "Zhejiang University",
-    date: "Dec 20, 2024",
-    invited: "kkk",
-    keynote: true, 
-    discussant: true
+    date: "May 28, 2025",
+    invited: "Prof. Jiajun Bu", 
+    link: "https://mp.weixin.qq.com/s/WdyWaX0u0Wd4HceLb1FBYQ"
   },
   {
-    conference: "Workshop on e-Business (WeB)",
-    title: "Large Language Model In Creative Work",
-    award: "Best Completed Research Paper Runner-up",
-    link: "https://credsverse.com/credentials/982584eb-2d56-47a5-b604-d0e7ef6b5eea",
-    location: "Online",
-    keynote: true, 
-    date: "Dec 9, 2023"
+    conference: "The 7th Yangtze-Huaihe River Information Technology Summit",
+    title: "From LLMs to Structured Scientific Intelligence", 
+    link: "https://mp.weixin.qq.com/s/dby1KIRhbP-Yxm2NZNYsIQ",
+    location: "Wuhu, Anhui", 
+    date: "May 25, 2025"
   },
-  {
-    conference: "HKU Business School",
-    title: "Large Language Model In Creative Work",
-    location: "Hong Kong, China",
-    date: "Nov 2, 2023",
-    keynote: false, 
+  { 
+    title: "Evolution of HPC Paradigm through Unified Matrix Computation: Computing the Future in the AI Era",
+    location: "University of Science and Technology of China (East Campus)",
+    date: "Dec 23, 2024", 
     link: "https://www.hkubs.hku.hk/event/how-large-language-model-in-creative-work-the-role-of-collaboration-modality-and-user-expertise/",
-    invited: "kkk"
+    invited: "Prof. Honghui Shang"
+  },
+  { 
+    title: "Evolution of HPC Paradigm through Unified Matrix Computation: Computing the Future in the AI Era",
+    location: "University of Science and Technology of China (Gaoxin Campus)",
+    date: "Dec 22, 2024", 
+    link: "https://cs.ustc.edu.cn/2024/1214/c14893a665361/page.htm",
+    invited: "Prof. Hong An"
+  },
+  { 
+    conference: "CCF HPC China 2024",
+    title: "Evolving the HPC Paradigm with Unified Matrix Computation on AI Accelerators",
+    location: "Wuhan, Hubei",
+    date: "Sep 25, 2024", 
+    link: "https://ccf.org.cn/hpcchina2024/speaker_2004",
+    invited: "Prof. Haibing Guan",
+    Keynote: true
+  },
+  { 
+    title: "A New High-Performance Computing Paradigm on Tensor Core Units",
+    location: "Lenovo Research",
+    date: "Sep 20, 2024",  
+    invited: "Yan Li"
+  },
+  { 
+    title: "Evolving the HPC Paradigm with Unified Matrix Computation on AI Accelerators",
+    location: "CNIC, CAS",
+    date: "Aug 30, 2024",  
+    invited: "Prof. Jue Wang"
+  },
+  { 
+    title: "Evolving the HPC Paradigm with Unified Matrix Computation on AI Accelerators",
+    location: "ICT, CAS",
+    date: "Aug 29, 2024",  
+    invited: "Prof. En Shao"
+  },
+  { 
+    title: "ConvStencil: Transform Stencil Computation to Matrix Multiplication on Tensor Cores",
+    location: "University of Rochester",
+    date: "Nov 19, 2023",  
+    invited: "Prof. Chen Ding"
+  },
+  { 
+    conference: "Star Roundtable",
+    location: "University of Chinese Academy of Sciences",
+    date: "Nov 18, 2023",  
+    invited: "GuoMeng Studio",
+    link: "https://mp.weixin.qq.com/s/u9cqTQ3iB-ESkQFMuO2U5A"
+  },
+  { 
+    conference: "AI4Science Salon",
+    location: "Peking University",
+    date: "Oct 28, 2023",  
+    invited: "PKU Linux Club",
+    link: "https://mp.weixin.qq.com/s/APVRieov0sYVSljr7swJOQ"
+  },
+  { 
+    conference: "CCF SPP Live",
+    location: "online",
+    date: "Oct 11, 2023",   
+  },
+  { 
+    conference: "CCF YEF 2023",
+    location: "Wenzhou, Zhejiang",
+    date: "May 18, 2023",  
+    invited: "Prof. Jianfei Chen",
+    link: "https://mp.weixin.qq.com/s/Zy24yLjyIsuGfD_MYn0C1A"
+  },
+  { 
+    conference: "The Young Scholars Forum",
+    location: "ICT, CAS",
+    date: "Apr 26, 2023",  
+    invited: "Prof. Liang Yuan",
+    link: "https://mp.weixin.qq.com/s/Zy24yLjyIsuGfD_MYn0C1A"
+  },
+  { 
+    conference: "The Young Scholars Forum",
+    location: "Shandong University",
+    date: "Apr 26, 2023",  
+    invited: "Prof. Liang Yuan",
+    link: "https://mp.weixin.qq.com/s/Zy24yLjyIsuGfD_MYn0C1A"
   },
 ];
