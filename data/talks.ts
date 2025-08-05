3d70aa77171ee47440f720073ae0c3fe8653e3ff
kunli <kunli@microsoft.com>
2025-08-05 18:01:44 +0800

    update

--- data/talks.ts
+++ data/talks.ts
@@ -5,6 +5,7 @@ export const talks = [
     location: "Zhejiang University",
     date: "Dec 20, 2024",
     invited: "kkk",
+    keynote: true, 
     discussant: true
   },
   {
@@ -13,6 +14,7 @@ export const talks = [
     award: "Best Completed Research Paper Runner-up",
     link: "https://credsverse.com/credentials/982584eb-2d56-47a5-b604-d0e7ef6b5eea",
     location: "Online",
+    keynote: true, 
     date: "Dec 9, 2023"
   },
   {
@@ -20,147 +22,8 @@ export const talks = [
     title: "Large Language Model In Creative Work",
     location: "Hong Kong, China",
     date: "Nov 2, 2023",
+    keynote: false, 
     link: "https://www.hkubs.hku.hk/event/how-large-language-model-in-creative-work-the-role-of-collaboration-modality-and-user-expertise/",
     invited: "kkk"
   },
-  {
-    conference: "CUHK Business School",
-    title: "Large Language Model In Creative Work",
-    location: "Hong Kong, China",
-    date: "Oct 30, 2023",
-    link: "https://www.bschool.cuhk.edu.hk/events/large-language-model-in-creative-work-the-role-of-collaboration-modality-and-user-expertise/",
-    invited: "kkk"
-  },
-  {
-    conference: "Workshop on Information Systems and Economics (WISE)",
-    title: "Large Language Model In Creative Work",
-    location: "Online",
-    date: "Dec 13, 2023"
-  },
-  {
-    conference: "Conference on Artificial Intelligence, Machine Learning, and Business Analytics",
-    title: "Large Language Model In Creative Work",
-    location: "Philadelphia, Pannsylvania",
-    date: "Dec 1, 2023"
-  },
-  {
-    conference: "Conference on Digital Experimentation (CoDE) @ MIT",
-    title: "Large Language Model In Creative Work",
-    location: "Cambridge, Massachusetts",
-    date: "Nov 10, 2023"
-  },
-  {
-    conference: "Conference in Information Systems and Technology (CIST)",
-    title: "Large Language Model In Creative Work",
-    award: "Best Student Paper Award Runner-up",
-    location: "Phoenix, Arizona",
-    date: "Oct 14, 2023"
-  },
-  {
-    conference: "INFORMS Annual Meeting",
-    title: "Large Language Model In Creative Work",
-    location: "Phoenix, Arizona",
-    date: "Oct 15, 2023"
-  },
-  {
-    conference: "Business & Generative AI Workshop @ Wharton",
-    title: "Large Language Model In Creative Work",
-    location: "San Francisco, California",
-    date: "Sep 8, 2023"
-  },
-  {
-    conference: "International Conference on Information Systems (ICIS)",
-    title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
-    award: "Best Paper in General IS Track",
-    location: "Copenhagen, Denmark",
-    date: "Dec 13, 2022",
-    link: "https://aisel.aisnet.org/icis2022/general_is/general_is/1/"
-  },
-  {
-    conference: "Conference in Information Systems and Technology (CIST)",
-    title: "Net Neutrality: Network Congestion, Social Welfare and Service Tiering Regulations",
-    location: "Seattle, Washington",
-    date: "Oct 19, 2019",
-  },
-  {
-    conference: "Conference in Information Systems and Technology (CIST)",
-    title: "Investigating the Coordination of Functionality Richness, First-party Content Quality, and Revenue-sharing on Digital Platforms",
-    location: "Virtual",
-    date: "Nov 8, 2020"
-  },
-  {
-    conference: "Conference in Information Systems and Technology (CIST)",
-    title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
-    location: "Indianapolis, Indiana",
-    date: "Oct 16, 2022"
-  },
-  {
-    conference: "Workshop on Information Technologies and Systems (WITS)",
-    title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
-    award: "Best Student Paper Award",
-    location: "Copenhagen, Denmark",
-    date: "Dec 14, 2022",
-    link: "https://witsconf.org/wits2022-awards/"
-  },
-  {
-    conference: "Workshop on Information Technologies and Systems (WITS)",
-    title: "Investigating the Coordination of Functionality Richness, First-party Content Quality, and Revenue-sharing on Digital Platforms",
-    location: "Austin, Texas",
-    date: "Dec 16, 2021"
-  },
-  {
-    conference: "Symposium on Statistical Challenges in Electronic Commerce Research (SCECR)",
-    title: "Attendance Tracking Apps Academic Performance: Evidence from a Field Study",
-    location: "Virtual",
-    date: "Jun 17, 2021"
-  },
-  {
-    conference: "Symposium on Statistical Challenges in Electronic Commerce Research (SCECR)",
-    title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
-    location: "Madrid, Spain",
-    date: "Jun 21, 2022"
-  },
-  {
-    conference: "Platform Strategy Symposium",
-    title: "Investigating the Coordination of Functionality Richness, First-party Content Quality, and Revenue-sharing on Digital Platforms",
-    location: "Boston, Massachusetts",
-    link: "https://www.youtube.com/watch?v=iQK2ZKabHdI",
-    date: "Jul 19, 2022"
-  },
-  {
-    conference: "Conference on Digital Experimentation (CoDE) @ MIT",
-    title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
-    location: "Cambridge, Massachusetts",
-    date: "Oct 21, 2022"
-  },
-  {
-    conference: "Americas Conference on Information Systems (AMCIS)",
-    title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
-    location: "Minneapolis, Minnesota",
-    date: "Aug 13, 2022"
-  },
-  {
-    conference: "INFORMS Annual Meeting",
-    title: "Investigating the Coordination of Functionality Richness, First-party Content Quality , and Revenue-sharing on Digital Platforms",
-    location: "Indianapolis, Indiana",
-    date: "Oct 18, 2022"
-  },
-  {
-    conference: "China Workshop on Economics of Information Systems Theory (CWEIST)",
-    title: "Dynamics of User Preference in Broadband Market",
-    location: "Virtual",
-    date: "Jun 30, 2020"
-  },
-  {
-    conference: "China Workshop on Economics of Information Systems Theory (CWEIST)",
-    title: "Investigating the Coordination of Functionality Richness, First-party Content Quality, and Revenue-sharing on Digital Platforms",
-    location: "Virtual",
-    date: "Jun 28, 2021"
-  },
-  {
-    conference: "Industry Studies Association (ISA) Annual Conference",
-    title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
-    location: "Philadelphia, Pennsylvania",
-    date: "Jun 25, 2022"
-  }
 ];
