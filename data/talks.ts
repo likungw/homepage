ce0f215051d6fb88c2ac1394bf4d5870130cb979
kunli <kunli@microsoft.com>
2025-08-06 10:28:58 +0800

    update

--- data/talks.ts
+++ data/talks.ts
@@ -4,14 +4,14 @@ export const talks: Talk[] = [
   { 
     title: "Opportunities and Challenges in the Development of Internet Application Technologies", 
     location: "Beijing",
-    date: "Jul 16, 2025",
+    date: "2025-07-16",
     invited: "CCF TCHPC",
     keynote: false
   },
   { 
     title: "High-Performance Computing Empowering New Advances in Industry-Academia-Research Collaboration", 
     location: "Alibaba Group",
-    date: "Aug 7, 2025",
+    date: "2025-08-07",
     invited: "CCF TCHPC",
     keynote: false
   },
@@ -20,7 +20,7 @@ export const talks: Talk[] = [
     title: "Forum on Efficient Training and Inference of Large Models",
     link: "http://youth.csig.org.cn/CSIG2025/views/dhlt.html",
     location: "Qingdao, Shandong",
-    date: "Sep 19, 2025",
+    date: "2025-09-19",
     invited: "Prof. Zhiyong Yang@UCAS",
     keynote: false
   },
@@ -29,7 +29,7 @@ export const talks: Talk[] = [
     title: "The 7th Forum on Architectures, Algorithms, and Applications of High-Performance Sparse Matrix Computation",
     link: "https://ccf.org.cn/hpcchina2025/schedule_d_2016",
     location: "Ordos, Inner Mongolia",
-    date: "Aug 15, 2025",
+    date: "2025-08-15",
     invited: "Prof. Weifeng Liu@CUP and Prof. Guangming Tan@ICT,CAS",
     keynote: false
   },
@@ -39,7 +39,7 @@ export const talks: Talk[] = [
     link: "https://ccf.org.cn/hpcchina2025/schedule_d_2015",
     location: "Ordos, Inner Mongolia",
     invited: "Prof. Jingwei Sun@USTC",
-    date: "Aug 16, 2025",
+    date: "2025-08-16",
     keynote: false
   },
   {
@@ -47,14 +47,14 @@ export const talks: Talk[] = [
     title: "The 3rd Forum on Foundational Libraries for Domestic Chips",
     link: "https://ccf.org.cn/hpcchina2025/schedule_d_2007",
     location: "Ordos, Inner Mongolia",
-    date: "Aug 16, 2025",
+    date: "2025-08-16",
     invited: "Prof. Chun Huang@NUDT",
     keynote: false
   },
   {
     title: "Towards E-Level Scalable Scientific Intelligence Systems",
     location: "Hangzhou, Zhejiang",
-    date: "May 28, 2025",
+    date: "2025-05-28",
     invited: "Prof. Jiajun Bu@ZJU",
     link: "https://mp.weixin.qq.com/s/WdyWaX0u0Wd4HceLb1FBYQ",
     keynote: false
@@ -64,14 +64,14 @@ export const talks: Talk[] = [
     title: "From LLMs to Structured Scientific Intelligence",
     link: "https://mp.weixin.qq.com/s/dby1KIRhbP-Yxm2NZNYsIQ",
     location: "Wuhu, Anhui",
-    date: "May 25, 2025",
+    date: "2025-05-25",
     invited: "CCF TCHPC",
     keynote: false
   },
   {
     title: "Evolution of HPC Paradigm through Unified Matrix Computation: Computing the Future in the AI Era",
     location: "Hefei, Anhui",
-    date: "Dec 23, 2024",
+    date: "2024-12-23",
     link: "https://www.hkubs.hku.hk/event/how-large-language-model-in-creative-work-the-role-of-collaboration-modality-and-user-expertise/",
     invited: "Prof. Honghui Shang@USTC",
     keynote: false
@@ -79,7 +79,7 @@ export const talks: Talk[] = [
   {
     title: "Evolution of HPC Paradigm through Unified Matrix Computation: Computing the Future in the AI Era",
     location: "Hefei, Anhui",
-    date: "Dec 22, 2024",
+    date: "2024-12-22",
     link: "https://cs.ustc.edu.cn/2024/1214/c14893a665361/page.htm",
     invited: "Prof. Hong An@USTC",
     keynote: false
@@ -88,7 +88,7 @@ export const talks: Talk[] = [
     conference: "CCF HPC China 2024",
     title: "Evolving the HPC Paradigm with Unified Matrix Computation on AI Accelerators",
     location: "Wuhan, Hubei",
-    date: "Sep 25, 2024",
+    date: "2024-09-25",
     link: "https://ccf.org.cn/hpcchina2024/speaker_2004",
     invited: "Prof. Haibing Guan@SJTU",
     keynote: true
@@ -96,35 +96,35 @@ export const talks: Talk[] = [
   {
     title: "A New High-Performance Computing Paradigm on Tensor Core Units",
     location: "Beijing",
-    date: "Sep 20, 2024",
+    date: "2024-09-20",
     invited: "Yan Li@Lenovo Research",
     keynote: false
   },
   {
     title: "Evolving the HPC Paradigm with Unified Matrix Computation on AI Accelerators",
     location: "Beijing",
-    date: "Aug 30, 2024",
+    date: "2024-08-30",
     invited: "Prof. Jue Wang@CNIC,CAS",
     keynote: false
   },
   {
     title: "Evolving the HPC Paradigm with Unified Matrix Computation on AI Accelerators",
     location: "Beijing",
-    date: "Aug 29, 2024",
+    date: "2024-08-29",
     invited: "Prof. En Shao@ICT,CAS",
     keynote: false
   },
   {
     title: "ConvStencil: Transform Stencil Computation to Matrix Multiplication on Tensor Cores",
     location: "online",
-    date: "Nov 19, 2023",
+    date: "2023-11-19",
     invited: "Prof. Chen Ding@University of Rochester",
     keynote: false
   },
   {
     conference: "Star Roundtable",
     location: "University of Chinese Academy of Sciences",
-    date: "Nov 18, 2023",
+    date: "2023-11-18",
     invited: "GuoMeng Studio@UCAS",
     link: "https://mp.weixin.qq.com/s/u9cqTQ3iB-ESkQFMuO2U5A",
     keynote: false
@@ -132,7 +132,7 @@ export const talks: Talk[] = [
   {
     conference: "AI4Science Salon",
     location: "Peking University",
-    date: "Oct 28, 2023",
+    date: "2023-10-28",
     invited: "PKU Linux Club",
     link: "https://mp.weixin.qq.com/s/APVRieov0sYVSljr7swJOQ",
     keynote: false
@@ -140,13 +140,13 @@ export const talks: Talk[] = [
   {
     conference: "CCF SPP Live",
     location: "online",
-    date: "Oct 11, 2023",
+    date: "2023-10-11",
     keynote: false
   },
   {
     conference: "CCF YEF 2023",
     location: "Wenzhou, Zhejiang",
-    date: "May 18, 2023",
+    date: "2023-05-18",
     invited: "Prof. Jianfei Chen@THU",
     link: "https://mp.weixin.qq.com/s/Zy24yLjyIsuGfD_MYn0C1A",
     keynote: false
@@ -154,14 +154,14 @@ export const talks: Talk[] = [
   {
     conference: "The Young Scholars Forum",
     location: "Beijing",
-    date: "Apr 26, 2023",
+    date: "2023-04-26",
     invited: "Prof. Liang Yuan@ICT,CAS",
     keynote: false
   },
   {
     conference: "The Young Scholars Forum",
     location: "Qingdao, Shandong",
-    date: "Mar 23, 2023",
+    date: "2023-03-23",
     invited: "Prof. Haisen Zhao@SDU",
     link: "http://www.tsxt.sdu.edu.cn/info/1010/5991.htm",
     keynote: false
@@ -169,7 +169,7 @@ export const talks: Talk[] = [
   {
     conference: "CCF HPC China 2022",
     location: "online",
-    date: "Dec 15, 2022",
+    date: "2022-12-15",
     invited: "Prof. Jidong Zhai@THU",
     link: "https://mp.weixin.qq.com/s/naA8OFoUayQV1akx7vQePg",
     keynote: false
