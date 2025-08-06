8d98caaa0dea3f6746ff4b672a77546fb964b0ce
kunli <kunli@microsoft.com>
2025-08-06 10:32:27 +0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,7 @@ export default function Home({ posts, projects, publications }: HomeProps) {
               <br />
               <br />
               <p>
-                Dr. Kun Li received his Ph.D. degree in Computer Architecture from the State Key Laboratory of Computer Architecture, 
+                Dr. Kun Li received his Ph.D. degree from the State Key Laboratory of Computer Architecture, 
                 Institute of Computing Technology, Chinese Academy of Sciences (ICT, CAS) in 2022. He has been recognized with numerous honors, including the <strong>CCF Outstanding Doctoral Dissertation Award</strong>, <strong>ACM SIGHPC China Outstanding Doctoral Dissertation Award</strong>,  <strong>CCF Youth Talent Award in HPC</strong>, and <strong>ACM SIGHPC China Rising Star Award</strong>. His work has been published in top-tier CCF-A conferences such as <strong>SC</strong>, <strong>PPoPP</strong>, <strong>ATC</strong>, <strong>ASPLOS</strong>, and <strong>ICS</strong>, earning the <strong>Best Paper Award at PPoPP’24</strong>, <strong>SC’25 Best Student Paper Award Finalist</strong>, and <strong>SC’25 Reproducibility Challenge Candidate</strong>. 
               </p>
 
