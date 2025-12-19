590b48f89b0bba0a01debc8b4cbb9a5568fd34b6
kunli <kunli@microsoft.com>
2025-12-19 10:51:22 +0800

    update

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 type Props = React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
-export default function ExternalLink({ children, ...props }: Props) {
+export default function ExternalLink({ children, className = "", ...props }: Props) {
   return (
-    <a {...props} target="_blank" rel="noopener noreferrer">
+    <a
+      {...props}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`underline underline-offset-4 hover:opacity-80 transition ${className}`}
+    >
       {children}
     </a>
   );
